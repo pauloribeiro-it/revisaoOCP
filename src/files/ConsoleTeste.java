453427bebf73395853f4cd596f2cdf65c482a164
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-19 21:15:46 -0200

    Teste com console

---
+++ src/files/ConsoleTeste.java
@@ -0,0 +1,26 @@
+package files;
+
+import java.io.Console;
+
+public class ConsoleTeste {
+	public static void main(String[] args) {
+		String name = "";
+		Console c = System.console(); // #1: get a Console
+		char[] pw;
+		pw = c.readPassword("%s", "pw: "); // #2: return a char[]
+		for (char ch : pw)
+			c.format("%c ", ch); // #3: format output
+		c.format("\n");
+		MyUtility mu = new MyUtility();
+		while (true) {
+			name = c.readLine("%s", "input?: "); // #4: return a String
+			c.format("output: %s \n", mu.doStuff(name));
+		}
+	}
+}
+
+class MyUtility { // #5: class to test
+	String doStuff(String arg1) {
+		return "result is " + arg1;
+	}
+}
