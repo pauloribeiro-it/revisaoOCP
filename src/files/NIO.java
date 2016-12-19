fde7951b876f75cf28cbb32ac72d24d5e070155c
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-19 21:49:46 -0200

    Utilizando uri como path

--- src/files/NIO.java
+++ src/files/NIO.java
@@ -1,12 +1,13 @@
 package files;
 
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class NIO {
 	public static void main(String[] args) {
-		
+		verificaExistenciaArquivo();
 	}
 	
 	public static void verificaExistenciaArquivo(){
@@ -14,7 +15,10 @@ public class NIO {
 		String way2[] = {"C:","Users","Paulo","Desktop","Estudos","Java","NIO","teste.txt"}; 
 		Path arquivo1 = Paths.get(way1);
 		Path arquivo2 = Paths.get(way2[0],way2[1],way2[2],way2[3],way2[4],way2[5],way2[6],way2[7]);
+		Path arquivo3 = Paths.get(URI.create("file:///C:/Users/Paulo/Desktop/Estudos/Java/NIO/teste.txt"));
 		System.out.println(Files.exists(arquivo1));
 		System.out.println(Files.exists(arquivo2));
+		System.out.println(Files.exists(arquivo3));
+		
 	}
 }
