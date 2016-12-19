d8662be5dabc60455d2e4f147efa3b57476301b6
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-19 21:27:23 -0200

    NIO

---
+++ src/files/NIO.java
@@ -0,0 +1,20 @@
+package files;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIO {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void verificaExistenciaArquivo(){
+		String way1 = "C:\\Users\\Paulo\\Desktop\\Estudos\\Java\\NIO\\teste.txt";
+		String way2[] = {"C:","Users","Paulo","Desktop","Estudos","Java","NIO","teste.txt"}; 
+		Path arquivo1 = Paths.get(way1);
+		Path arquivo2 = Paths.get(way2[0],way2[1],way2[2],way2[3],way2[4],way2[5],way2[6],way2[7]);
+		System.out.println(Files.exists(arquivo1));
+		System.out.println(Files.exists(arquivo2));
+	}
+}
