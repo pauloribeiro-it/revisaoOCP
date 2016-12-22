c25ffe7119c6f073d702880187982c29adac06fc
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-22 21:47:35 -0200

    BasicFileAttribute View

--- src/files/FilesAttributtes.java
+++ src/files/FilesAttributtes.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 
 public class FilesAttributtes {
 	public static void main(String[] args) throws IOException {
@@ -13,9 +15,27 @@ public class FilesAttributtes {
 
 	public static void useBasicFileAttributtes() throws IOException {
 		Path path = Paths.get("file.txt");
-		Files.createFile(path);
-		BasicFileAttributes bfa = Files.readAttributes(path,BasicFileAttributes.class);
-		System.out.println("Creation time: "+bfa.creationTime());
-		System.out.println("Last time modified: "+bfa.lastModifiedTime());
+		if (Files.exists(path))
+			Files.createFile(path);
+		BasicFileAttributes bfa = Files.readAttributes(path,
+				BasicFileAttributes.class);
+		System.out.println("Creation time: " + bfa.creationTime());
+		System.out.println("Last time modified: " + bfa.lastModifiedTime());
+	}
+
+	public static void useBasicFileAttributtesView() throws IOException {
+		Path path = Paths.get("file.txt");
+		if (Files.exists(path))
+			Files.createFile(path);
+		
+		BasicFileAttributes bfa = Files.readAttributes(path,
+				BasicFileAttributes.class);
+		
+		BasicFileAttributeView basic = Files.getFileAttributeView(path,
+				BasicFileAttributeView.class);
+		FileTime lastUpdated = bfa.lastAccessTime(); 
+		FileTime created = bfa.creationTime();
+		FileTime now = FileTime.fromMillis(System.currentTimeMillis());
+		basic.setTimes(lastUpdated, created, now);
 	}
 }
