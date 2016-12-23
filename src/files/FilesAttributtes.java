50b3ca3271fed122bb5b3f3dfb16a55aa257c412
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-22 22:20:53 -0200

    Attribute view

--- src/files/FilesAttributtes.java
+++ src/files/FilesAttributtes.java
@@ -6,16 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
 import java.nio.file.attribute.FileTime;
 
 public class FilesAttributtes {
 	public static void main(String[] args) throws IOException {
-		useBasicFileAttributtes();
+		//useBasicFileAttributtes();
+//		useDosFileAttribute(true, true);
+		useDosFileAttribute(false, false);
 	}
 
 	public static void useBasicFileAttributtes() throws IOException {
 		Path path = Paths.get("file.txt");
-		if (Files.exists(path))
+		if (!Files.exists(path))
 			Files.createFile(path);
 		BasicFileAttributes bfa = Files.readAttributes(path,
 				BasicFileAttributes.class);
@@ -25,7 +29,7 @@ public class FilesAttributtes {
 
 	public static void useBasicFileAttributtesView() throws IOException {
 		Path path = Paths.get("file.txt");
-		if (Files.exists(path))
+		if (!Files.exists(path))
 			Files.createFile(path);
 		
 		BasicFileAttributes bfa = Files.readAttributes(path,
@@ -37,5 +41,29 @@ public class FilesAttributtes {
 		FileTime created = bfa.creationTime();
 		FileTime now = FileTime.fromMillis(System.currentTimeMillis());
 		basic.setTimes(lastUpdated, created, now);
+	}
+	
+	public static void useDosFileAttribute(boolean readOnly,boolean hidden) throws IOException{
+		Path path = Paths.get("dosFile.txt");
+		
+		if(!Files.exists(path))
+			Files.createFile(path);
+		
+		Files.setAttribute(path, "dos:hidden", hidden);
+		Files.setAttribute(path, "dos:readonly", readOnly);
+		DosFileAttributes dfa = Files.readAttributes(path, DosFileAttributes.class);
+		System.out.println("IsHidden: "+dfa.isHidden());
+		System.out.println("ReadOnly: "+dfa.isReadOnly());
+	}
+	
+	public static void useDosFileAttributeView(boolean readOnly,boolean hidden) throws IOException{
+		Path path = Paths.get("dosFile.txt");
+		
+		if(!Files.exists(path))
+			Files.createFile(path);
+		
+		DosFileAttributeView dfav = Files.getFileAttributeView(path, DosFileAttributeView.class);
+		dfav.setHidden(hidden);
+		dfav.setReadOnly(readOnly);
 	}
 }
