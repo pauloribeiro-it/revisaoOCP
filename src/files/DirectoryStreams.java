31014dfa55bf02a2cab5f289d2d10217aad00a3a
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-23 23:09:47 -0200

    Ending DirectoryStream

--- src/files/DirectoryStreams.java
+++ src/files/DirectoryStreams.java
@@ -1,21 +1,39 @@
 package files;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DirectoryStreams {
+	private static final String DIRECTORY_NAME = "directory1";
+	
 	public static void main(String[] args) throws IOException {
 		createDirectoryAndFiles();
+		createDirectoryStream("v*");
+		System.out.println("-----");
+		createDirectoryStream("w*");
 	}
 	
 	public static void createDirectoryAndFiles() throws IOException{
-		Path directory1 = Paths.get("directory1");
-		if(!Files.exists(directory1)){
-			Files.createDirectory(directory1);
-		}
-		Files.createDirectory(Paths.get("directory1","subdirectory1"));
-		Files.createDirectory(Paths.get("directory1","subdirectory2"));
+		Path directory1 = Paths.get(DIRECTORY_NAME);
+		verifyDirectoryExistence(directory1);
+		verifyDirectoryExistence(Paths.get(DIRECTORY_NAME,"vsubdirectory1"));
+		verifyDirectoryExistence(Paths.get(DIRECTORY_NAME,"wsubdirectory2"));
 	}
+	
+	public static void createDirectoryStream(String expression) throws IOException{
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(DIRECTORY_NAME),expression)){
+			for(Path path:stream)
+				System.out.println(path);
+		}
+	}
+	
+	private static final void verifyDirectoryExistence(Path directory) throws IOException{
+		if(!Files.exists(directory)){
+			Files.createDirectory(directory);
+		}
+	}
+	
 }
