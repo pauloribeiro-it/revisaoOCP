10f6958296e37f6364582abe8dcb51344f949e0e
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2016-12-26 21:55:17 -0200

    File visitor navegação

---
+++ src/filevisitor/FileVisitorTeste2.java
@@ -0,0 +1,55 @@
+package filevisitor;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileVisitorTeste2 extends SimpleFileVisitor<Path> {
+	private static final String PATH = "home";
+	private static final String PATH2 = "child";
+
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+		System.out.println("pre: " + dir);
+		return FileVisitResult.CONTINUE;
+	}
+
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+		System.out.println("file: " + file);
+		return FileVisitResult.CONTINUE;
+	}
+
+	public FileVisitResult visitFileFailed(Path file, IOException exc) {
+		return FileVisitResult.CONTINUE;
+	}
+
+	public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+		System.out.println("post: " + dir);
+		return FileVisitResult.CONTINUE;
+	}
+	
+	private static void createDirectoriesAndFiles() throws IOException {
+		// TODO Revisar isso aqui
+		Files.createDirectories(Paths.get(PATH));
+		createFile(Paths.get(PATH, "a.txt"));
+		Files.createDirectories(Paths.get(PATH, "emptychild"));
+		Files.createDirectories(Paths.get(PATH, PATH2));
+		createFile(Paths.get(PATH, PATH2, "b.txt"));
+		Files.createDirectories(Paths.get(PATH, PATH2, "grandchild"));
+		Files.createFile(Paths.get(PATH, PATH2, "grandchild", "c.txt"));
+	}
+
+	private static void createFile(Path file) throws IOException {
+		if (Files.exists(file)) {
+			Files.createFile(file);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+//		createDirectoriesAndFiles();
+		Files.walkFileTree(Paths.get(PATH), new FileVisitorTeste2());
+	}
+}
