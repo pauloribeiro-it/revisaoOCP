f5b013e6e6528662aa3bc676bfedf381131316e6
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-01-23 20:19:19 -0200

    JDBC

---
+++ src/jdbc/bd_revisaoocp.sql
@@ -0,0 +1,10 @@
+create database revisaoocp;
+use revisaoocp;
+create table pessoa(
+    name character varying(100),
+    age int
+);
+insert into pessoa (name,age) values("paulo",23);
+insert into pessoa (name,age) values("luciana",20);
+insert into pessoa (name,age) values("isaac",22);
+insert into pessoa (name,age) values("rafaela",22);
