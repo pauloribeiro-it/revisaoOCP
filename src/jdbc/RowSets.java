8f261b7226d983c859b2579d670ab4c4be469fcb
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-01-25 19:08:21 -0200

    Row set connected

--- src/jdbc/RowSets.java
+++ src/jdbc/RowSets.java
@@ -1,5 +1,10 @@
 package jdbc;
 
+import java.sql.SQLException;
+
+import javax.sql.RowSet;
+import javax.sql.RowSetEvent;
+import javax.sql.RowSetListener;
 import javax.sql.rowset.JdbcRowSet;
 import javax.sql.rowset.RowSetProvider;
 
@@ -8,17 +13,50 @@ public class RowSets {
 	private static final String USER_NAME = "root";
 	private static final String PASSWORD = "root";
 	private static final String FIND_ALL = "select * from person";
-	public static void main(String[] args) throws Exception{
-		try(JdbcRowSet jrs = RowSetProvider.newFactory().createJdbcRowSet()){
+
+	public static void main(String[] args) throws Exception {
+		try (JdbcRowSet jrs = RowSetProvider.newFactory().createJdbcRowSet()) {
 			jrs.setUrl(URL_CONNECTION);
 			jrs.setUsername(USER_NAME);
 			jrs.setPassword(PASSWORD);
 			jrs.setCommand(FIND_ALL);
+			jrs.addRowSetListener(new RowSetListenerExample());
 			jrs.execute();
-			while(jrs.next()){
-				System.out.println("Name: "+jrs.getString("name"));
+			while (jrs.next()) {
+				System.out.println("Name: " + jrs.getString("name"));
 			}
 		}
-		
+
 	}
+}
+
+class RowSetListenerExample implements RowSetListener {
+
+	@Override
+	public void rowSetChanged(RowSetEvent event) {
+		System.out.println("RowSet changed");
+	}
+
+	@Override
+	public void rowChanged(RowSetEvent event) {
+		if (event.getSource() instanceof RowSet) {
+			try {
+				((RowSet) event.getSource()).execute(); 
+			} catch (SQLException se) {
+				System.out.println("SQLException during execute");
+			}
+		}
+	}
+
+	@Override
+	public void cursorMoved(RowSetEvent event) {
+		if (event.getSource() instanceof RowSet) {
+			try {
+				System.out.println("Cursos moved to "+((RowSet) event.getSource()).getRow()+" row.");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
